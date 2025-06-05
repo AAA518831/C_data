d5625551dc3894d5b2b9b070d56c92864a0f78bb
AAA518831 <goy2k327@gmail.com>
2025-06-05 11:47:36 +0900

    구조체의 배열

---
+++ struct/struct-04.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+struct student {
+	int number;
+	char name[20];
+	double grade;
+};
+
+int main(void) {
+	struct student list[100];     //구조체의 배열 선언
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		printf("학번을 입력하시오:");
+		scanf("%d", &list[i].number);
+		printf("이름을 입력하시오:");
+		scanf("%s", &list[i].name);
+		printf("학점을 입력하시오:");
+		scanf("%lf", &list[i].grade);
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		printf("학변: %d  이름: %s  학점: %.1f\n", list[2].number, list[2].name, list[2].grade);
+	}
+	/*printf("%d\n", list[2].number);
+	printf("%s\n", list[2].name);
+	printf("%f", list[2].grade);*/
+
+	return 0;
+}
