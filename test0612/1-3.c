d5070467dd6125c49f8920a1461c315de74d8ac0
AAA518831 <goy2k327@gmail.com>
2025-06-12 11:35:06 +0900

    while문 복습.

---
+++ test0612/1-3.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	char class; //반
+	int num; //번호
+	int c; //c언어 점수
+	int java; //자바 점수
+	int pyth; //파이썬 점수
+	int tot; //총점
+	double avg; //평균점수
+	char e; //y 또는 n
+	int k = 1;
+	while (k)
+	{
+		printf("학번을 입력하세요 : ");
+		scanf("%d", &num);
+		printf("반(A~C)을 입력하세요 : ");
+		scanf(" %c", &class);
+		printf("c언어 점수를 입력하세요 : ");
+		scanf("%d", &c);
+		printf("자바 점수를 입력하세요 : ");
+		scanf("%d", &java);
+		printf("파이썬 점수를 입력하세요 : ");
+		scanf("%d", &pyth);
+
+		tot = c + java + pyth;
+		avg = tot / (double)3;
+		printf("총점: %d\t 평균: %.2f\n", tot, avg);
+
+		printf("----------------------------------------------\n");
+		printf("계산을 종료하시겠습니까? (y/n) : "); 
+		scanf(" %c", &e);
+
+		if (e == 'y' || e == 'Y')
+		{
+			k = 0;
+		} //if 괄호
+	} //while 괄호
+
+	printf("종료합니다\n");
+
+	return 0;
+}
